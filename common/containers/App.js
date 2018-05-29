ce0f3ce9748452995260c8ba75ba7d6fddf95b4f
Hao Tran <HaoCSE@users.noreply.github.com>
2018-05-29 09:49:46 +0700

    Init...

---
+++ common/containers/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+// import { Component } from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Counter from '../components/Counter'
+import * as CounterActions from '../actions'
+
+const mapStateToProps = (state) => ({
+  counter: state.counter
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(CounterActions, dispatch)
+}
+
+
+import { BlockMath, InlineMath } from 'react-katex';
+
+@connect(
+  mapStateToProps,
+  { ...CounterActions }
+)
+export default
+class App extends Component {
+
+  render () {
+      return (
+        <div>
+          This is an in-line expression <InlineMath math={String.raw`\frac{1-\left(\frac{1}{2}\right)^n}{2}`} />{' '}
+          passed as <code>math prop</code>. This is an in-line{' '}
+          <InlineMath math={'\\int_0^\\infty x^2 dx'} /> expression passed as{' '}
+          <code>children prop</code>.
+      </div>
+      )
+  }
+}
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter)
